ffd66e824c60e1ff78cecaddfe5479d1adbf60f8
MaiaAndrey31 <maiaandrey31@outlook.com>
2023-11-23 22:37:10 -0300

    featEntityCategory

---
+++ src/entities/category.entity.ts
@@ -0,0 +1,22 @@
+type CategoryProps = {
+    title: string
+    color: string
+    id: string
+}
+
+export class Category {
+    public id?: string
+    public title: string
+    public color: string
+
+
+
+    constructor( {id, color, title}: CategoryProps) {
+        this.id = id
+        this.title= title
+        this.color = color
+    }
+}
+
+
+
