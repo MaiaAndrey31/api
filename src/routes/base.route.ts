054b875922a3b47c33fa056cd9802c7ba2af7fd2
MaiaAndrey31 <maiaandrey31@outlook.com>
2023-11-29 19:15:59 -0300

    eslintCorrection

--- src/routes/base.route.ts
+++ src/routes/base.route.ts
@@ -1,16 +1,16 @@
-import {Router} from 'express'
+import {Router} from 'express';
 
-import packagejson from '../../package.json'
+import packageJson from '../../package.json';
 
 
-export const baseRoutes = Router()
+export const baseRoutes = Router();
 
 
 baseRoutes.get( '/',(_, res) =>{
 
-    const { name, version, description, author} = packagejson
+    const { name, version, description, author} = packageJson
 
     res.status(200).json({ name, version, description, author})
 
 
-} )
+} );
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-import 'dotenv/config'
-
-import express, { json } from 'express'
-import { routes } from './routes'
-import { setupMongo } from './database'
+import 'dotenv/config';
+import express, { json } from 'express';
+import { routes } from './routes';
+import { setupMongo } from './database';
 
 
 setupMongo().then(() => {
 
-    const app = express()
+    const app = express();
 
 
 
-app.use(json())
-app.use(routes)
+app.use(json());
+app.use(routes);
 
-app.listen(3131, () => console.log('🚀 server online'))
+app.listen(3131, () => console.log('🚀 server online'));
 
-})
+});
